fd6ae58e3ca31fd0cc6a67273d603c05066cd582
kiluazen <kushalsokke@gmail.com>
2024-09-22 22:34:11 +0530

    cleaner removed un-necessary websites

--- content.js
+++ content.js
@@ -1,23 +1,3 @@
-console.log("ChatSync Content Script Loaded");
-let buttonAdded = false;
-function downloadFile(content, filename, contentType) {
-    return new Promise((resolve, reject) => {
-        const blob = new Blob([content], { type: contentType });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.style.display = 'none';
-        a.href = url;
-        a.download = filename;
-        document.body.appendChild(a);
-        a.click();
-        setTimeout(() => {
-            document.body.removeChild(a);
-            URL.revokeObjectURL(url);
-            resolve();
-        }, 100);
-    });
-}
-
 function extractChatMessages() {
     let chatMessages = [];
     const currentURL = window.location.hostname;
@@ -51,7 +31,6 @@ function extractChatMessages() {
     
 
     const jsonChat = JSON.stringify(chatMessages, null, 2);
-    console.log("Extracted chat:", jsonChat);
     return { status: "success", data: jsonChat };
 
 }
@@ -73,56 +52,6 @@ function copyToClipboard(text) {
     }
 }
 
-function addFloatingButton() {
-    console.log('Adding floating button...');
-    const button = document.createElement('div');
-    button.id = 'chatSyncFloatingButton';
-    button.textContent = '🍄';
-    button.style.position = 'fixed';
-    button.style.zIndex = '9999';
-    document.body.appendChild(button);
-
-    button.addEventListener('click', () => {
-        const result = extractChatMessages();
-        if (result.status === "success") {
-            copyToClipboard(result.data);
-        } else {
-            showPopup('Failed to extract chat: ' + result.message);
-        }
-    });
-    button.addEventListener('DOMNodeRemovedFromDocument', handleButtonRemoved);
-    buttonAdded = true;
-    // button.addEventListener('click', () => {
-    //     const result = extractChatMessages();
-    //     if (result.status === "success") {
-    //         console.log("Sending copyToClipboard message to background");
-    //         chrome.runtime.sendMessage({action: "copyToClipboard", data: result.data}, (response) => {
-    //             if (chrome.runtime.lastError) {
-    //                 console.error("Error:", chrome.runtime.lastError);
-    //                 showPopup('Failed to copy chat to clipboard: ' + chrome.runtime.lastError.message);
-    //             } else if (response) { // Check if response is defined
-    //                 if (response.success) {
-    //                     showPopup('Chat copied! Use Cmd + V to paste the conversation wherever you want');
-    //                 } else {
-    //                     showPopup('Failed to copy chat to clipboard: ' + (response.error || 'Unknown error'));
-    //                 }
-    //             } else {
-    //                 showPopup('No response received from background script.');
-    //             }
-    //         });
-    //     } else {
-    //         showPopup('Failed to extract chat: ' + result.message);
-    //     }
-    // });
-  }
-function handleButtonRemoved(event) {
-    console.log('Fucking hell')
-    // addFloatingButton();
-    // Add your callback function code here
-    // You can perform any additional actions or logging here
-    // You can also access the removed button element using `event.target`
-}
-
 function showPopup(message) {
 const popup = document.createElement('div');
 popup.id = 'chatSyncPopup';
@@ -130,45 +59,19 @@ popup.textContent = message;
 document.body.appendChild(popup);
 
 setTimeout(() => {
-    popup.remove();
-}, 3000);
-}
+    popup.classList.add('show');
+  }, 100);
 
-function observeDOMChanges() {
-    const observer = new MutationObserver((mutations) => {
-        console.log('MutationObserver triggered');
-        console.log('MUTATIONS',mutations)
-        mutations.forEach((mutation) => {
-            // console.log(mutation)
-            if (!document.getElementById('chatSyncFloatingButton')) {
-                console.log('Button not found, adding it...');
-                addFloatingButton();
-            } else {
-                // Check if the button was removed from the DOM
-                const removedNodes = mutation.removedNodes;
-                console.log('Removed Nodes',removedNodes)
-                for (let i = 0; i < removedNodes.length; i++) {
-                    
-                    if (removedNodes[i].id === 'chatSyncFloatingButton') {
-                        console.log('Button removed from DOM');
-                        // Call your callback function here
-                        handleButtonRemoved();
-                    }
-                }
-            }
-        });
-    });
-
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-    });
+  setTimeout(() => {
+    // Remove the 'show' class to trigger the opacity transition
+    popup.classList.remove('show');
+    setTimeout(() => {
+      popup.remove();
+    }, 300);
+  }, 3000);
 }
 
 function addShadowFloatingButton(){
-    console.log('Adding Shadow floating button...');
-    
-
     const shadowHost = document.createElement('div');
     shadowHost.id = 'chatSyncFloatingButton';
     document.body.appendChild(shadowHost);
@@ -190,14 +93,9 @@ function addShadowFloatingButton(){
         }
     `;
 
-    // Append style to shadow root
     shadowRoot.appendChild(style);
 
     const button = document.createElement('div');
-    // button.textContent = '🍄';
-    // button.style.position = 'fixed';
-    // button.style.zIndex = '9999';
-    
     
     const img = document.createElement('img');
     img.id = 'gtcimage';
@@ -218,17 +116,12 @@ function addShadowFloatingButton(){
             showPopup('Failed to extract chat: ' + result.message);
         }
     });
-
-    button.addEventListener('DOMNodeRemovedFromDocument', handleButtonRemoved);
-    buttonAdded = true;
 }
 
-// observeDOMChanges();
-  // Call this function when the content script loads
+
 addShadowFloatingButton();
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 if (message.action === "extractChat") {
-    console.log("Received extractChat message");
     const result = extractChatMessages();
     sendResponse(result);
     return true;
